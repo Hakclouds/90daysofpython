4f2a8212c67226dde5b93da1f70ec5eabd5b1d49
Hakcloud1 <mustaphakareem111@gmail.com>
2025-01-07 23:07:38 +0100

    Day 2 commit: Type casting

--- day 2.py
+++ day 2.py
@@ -1,4 +1,4 @@
 name= input(" What is your name: ")
 age= int(input("How old are you: "))
 yearofbirth = 2025 - age
-print (f"Hi {name}, I saw you were born in {yearofbirth}. Welcome to the 90 days python challenge. I hope you see it to the end. ")
+print (f"Hi {name}, I saw you were born in the year {yearofbirth}. Welcome to the 90 days python challenge. I hope you see it to the end. ")
