354fdbf1873ecbcd70bd94aa62e52ed86632739e
Hakcloud1 <mustaphakareem111@gmail.com>
2025-01-07 22:55:22 +0100

    day 1 commit

---
+++ day 2.py
@@ -0,0 +1,4 @@
+name= input(" What is your name: ")
+age= int(input("How old are you: "))
+yearofbirth = 2025 - age
+print (f"Hi {name}, I saw you were born in {yearofbirth}. Welcome to the 90 days python challenge. I hope you see it to the end. ")
