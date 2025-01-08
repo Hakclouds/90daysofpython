b906d5bb5046d861c5efff7354d99ea5143ba87e
Hakcloud1 <mustaphakareem111@gmail.com>
2025-01-08 17:22:41 +0100

    day 3, conditonal statement commit

---
+++ day 3.py
@@ -0,0 +1,5 @@
+age = int (input("How old are you? Kindly write your age in number: "))
+if age >= 18:
+    print ("You are eligible to vote")
+else:
+    print("Unfortunately, you are not eligible to vote until you reach 18 years old")
