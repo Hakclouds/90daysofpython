2471edb33695e05b2488298a62f7f20a2c389a8b
Hakcloud1 <mustaphakareem111@gmail.com>
2025-01-11 23:20:34 +0100

    day 6, list and tuple

---
+++ day 6.py
@@ -0,0 +1,11 @@
+
+numbers = [5, 10, 15, 20, 25,7,3, 12, 8, 3, 7,13]
+
+sum_of_numbers = sum(numbers)
+
+
+average = sum_of_numbers / len(numbers)
+
+# Step 4: Print the sum and average
+print("Sum:", sum_of_numbers)
+print("Average:", average)
